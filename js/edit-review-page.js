1e2a734dcc8dc0442b82814d0c96d48d94989b7d
unknown <swkim960715@gmail.com>
2023-08-23 21:47:55 +0900

    Save

--- js/edit-review-page.js
+++ js/edit-review-page.js
@@ -1,3 +1,36 @@
+(() => {
+  window.addEventListener("DOMContentLoaded", async () => {
+    const nameInput = document.querySelector(
+      "form > section > article:nth-of-type(1) > input"
+    );
+    const scoreInput = document.querySelector(
+      "form > section > article:nth-of-type(4) > section:nth-of-type(1) > input"
+    );
+    const volInput = document.querySelector(
+      "form > section > article:nth-of-type(4) > section:nth-of-type(2) > input"
+    );
+    const aromaInput = document.querySelector(
+      "form > section > article:nth-of-type(5) > input"
+    );
+    const tasteInput = document.querySelector(
+      "form > section > article:nth-of-type(6) > input"
+    );
+    const finishInput = document.querySelector(
+      "form > section > article:nth-of-type(7) > input"
+    );
+
+    const query = window.location.search;
+    const response = await fetch(`http://127.0.0.1:8080/reviews${query}`);
+    const result = await response.json();
+    nameInput.value = result.name;
+    scoreInput.value = result.score;
+    volInput.value = result.vol;
+    aromaInput.value = result.aroma;
+    tasteInput.value = result.taste;
+    finishInput.value = result.finish;
+  });
+})();
+
 // input 값 검증
 (() => {
   const form = document.querySelector("form");
@@ -102,11 +135,11 @@
         selectOption.options[selectOption.selectedIndex].text
       } ${selectSpirit.options[selectSpirit.selectedIndex].text}`;
     }
-
+    console.log(window.location.search);
     // 검증이 완료 되었으므로 이 값들을 백엔드 서버쪽으로 fetch를 통해 post로 데이터 입력
-
-    const response = await fetch("http://127.0.0.1:8080/reviews", {
-      method: "POST",
+    const query = window.location.search;
+    const response = await fetch(`http://127.0.0.1:8080/reviews${query}`, {
+      method: "PUT",
       headers: {
         "content-type": "application/json",
       },
--- js/review-page.js
+++ js/review-page.js
@@ -1,33 +1,72 @@
+let page = 0;
+let page_size = 4;
+
+function createAlcObject(no, img) {
+  const div = document.createElement("div");
+
+  div.dataset.no = no;
+  div.innerHTML = /*html*/ `
+	<img src=${img} />
+	<button>Detail</button>
+	`;
+
+  return div;
+}
+
+async function getPageList(page) {
+  const response = await fetch(
+    `http://127.0.0.1:8080/reviews/paging?page=${page}&size=${page_size}`
+  );
+  const result = await response.json();
+
+  const listSection = document.querySelector(
+    "main > section > article:nth-of-type(1) > section"
+  );
+  listSection.innerHTML = "";
+
+  for (let item of result.content) {
+    listSection.append(createAlcObject(item.no, item.img));
+  }
+}
+
+(() => {
+  window.addEventListener("DOMContentLoaded", () => {
+    getPageList(0);
+  });
+})();
 // 모달 창
 (() => {
   const list = document.querySelector(
     "main > section > article:nth-of-type(1) > section"
   );
-  list.addEventListener("click", (e) => {
+  list.addEventListener("click", async (e) => {
     if (e.target.tagName === "BUTTON") {
+      const no = e.target.closest("div").dataset.no;
+      const response = await fetch(`http://127.0.0.1:8080/reviews?no=${no}`);
+      const result = await response.json();
       const modal = document.createElement("div");
       // 실제 데이터로 교체
       modal.innerHTML = /*html*/ `
-			<section data-no="0">
+			<section data-no="${result.no}">
 				<article>
-					<img src="./test.jpg" />
-					<button><a href="./view/edit-review-page.html?no=4">수정하기</a></button>
+					<img src=${result.img} />
+					<button><a href="./view/edit-review-page.html?no=${no}">수정하기</a></button>
 					<button>삭제하기</button>
 				</article>
 				<article>
-					<section>이름</section>
+					<section>${result.name}</section>
 					<section>
 						<div>주종</div>
-						<div>싱글몰트 위스키</div>
+						<div>${result.spirit}</div>
 					</section>
 					<section>
 						<article>
 							<div>Score</div>
-							<div>4/5</div>
+							<div>${result.score}/5</div>
 						</article>
 						<article>
 							<div>Alc by vol</div>
-							<div>40%</div>
+							<div>${result.vol}%</div>
 						</article>
 					</section>
 					<section>Tasting Notes</section>
@@ -35,17 +74,17 @@
 						<div>
 							<span>Aroma</span>
 							<span>-</span>
-							<span>바닐라, 당밀, 토피, 스위트 발사믹</span>
+							<span>${result.aroma}</span>
 						</div>
 						<div>
 							<span>Taste</span>
 							<span>-</span>
-							<span>초콜릿, 트러플, 오렌지 껍질, 건포도</span>
+							<span>${result.taste}</span>
 						</div>
 						<div>
 							<span>Finish</span>
 							<span>-</span>
-							<span>다크 초콜릿, 메이플 시럽, 데메라라 설탕</span>
+							<span>${result.finish}</span>
 						</div>
 					</section>
 					<button><i class="fa-solid fa-xmark"></i></button>
